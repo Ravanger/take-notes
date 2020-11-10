6bbc972afd077e51d0a3892fab5b10c0a70a929f
Ravanger <Boris.Rozzy@gmail.com>
2020-11-10 02:15:54 -0500

    cardUtil setters, error handling, and tests

--- util/cardUtil.test.ts
+++ util/cardUtil.test.ts
@@ -1,48 +1,79 @@
-// import { getCardDataArray } from "./cardUtil"
+import { setAllCardData, getAllCardData, getCardDataById } from "./cardUtil"
+import "fake-indexeddb/auto"
 
-// cardData.forEach((card) => {
-//   set(card.id, card)
-// })
+const cardData: CardDataType[] = [
+  {
+    id: "c",
+    title: "5 Food Apps Delivering the Best of Your City",
+    backgroundColor: "#814A0E",
+  },
+  {
+    id: "f",
+    title: "Arrange Your Apple Devices for the Gram",
+    backgroundColor: "#959684",
+  },
+  {
+    id: "a",
+    title: "Map Apps for the Superior Mode of Transport",
+    backgroundColor: "#5DBCD2",
+  },
+  {
+    id: "g",
+    title: "Our Pick of Apps to Help You Escape From Apps",
+    backgroundColor: "#8F986D",
+  },
+  {
+    id: "d",
+    title: "The Latest Ultra-Specific Photography Editing Apps",
+    backgroundColor: "#FA6779",
+  },
+  {
+    id: "h",
+    title: "100 Cupcake Apps for the Cupcake Connoisseur",
+    backgroundColor: "#282F49",
+  },
+  {
+    id: "e",
+    title: "Yes, They Are Sociopaths",
+    backgroundColor: "#AC7441",
+  },
+  {
+    id: "b",
+    title: "Seriously the Only Escape is the Stratosphere",
+    backgroundColor: "#CC555B",
+  },
+]
 
-// const cardData: CardDataType[] = [
-//   {
-//     id: "c",
-//     title: "5 Food Apps Delivering the Best of Your City",
-//     backgroundColor: "#814A0E",
-//   },
-//   {
-//     id: "f",
-//     title: "Arrange Your Apple Devices for the Gram",
-//     backgroundColor: "#959684",
-//   },
-//   {
-//     id: "a",
-//     title: "Map Apps for the Superior Mode of Transport",
-//     backgroundColor: "#5DBCD2",
-//   },
-//   {
-//     id: "g",
-//     title: "Our Pick of Apps to Help You Escape From Apps",
-//     backgroundColor: "#8F986D",
-//   },
-//   {
-//     id: "d",
-//     title: "The Latest Ultra-Specific Photography Editing Apps",
-//     backgroundColor: "#FA6779",
-//   },
-//   {
-//     id: "h",
-//     title: "100 Cupcake Apps for the Cupcake Connoisseur",
-//     backgroundColor: "#282F49",
-//   },
-//   {
-//     id: "e",
-//     title: "Yes, They Are Sociopaths",
-//     backgroundColor: "#AC7441",
-//   },
-//   {
-//     id: "b",
-//     title: "Seriously the Only Escape is the Stratosphere",
-//     backgroundColor: "#CC555B",
-//   },
-// ]
+console.table(JSON.parse(JSON.stringify(cardData)))
+test("Setting card data", async () => {
+  try {
+    await setAllCardData(cardData)
+  } catch (e) {
+    expect(e).toMatch("error")
+  }
+})
+
+test("Get all cards should return test data", async () => {
+  try {
+    const data = await getAllCardData()
+    expect(data).toHaveLength(8)
+    expect(data.map((a) => a.id).sort()).toEqual(
+      cardData.map((a) => a.id).sort()
+    )
+  } catch (e) {
+    expect(e).toMatch("error")
+  }
+})
+
+test("Get card with ID 'a'", async () => {
+  try {
+    const data = await getCardDataById("a")
+    expect(data).toEqual({
+      id: "a",
+      title: "Map Apps for the Superior Mode of Transport",
+      backgroundColor: "#5DBCD2",
+    })
+  } catch (e) {
+    expect(e).toMatch("error")
+  }
+})
--- util/cardUtil.ts
+++ util/cardUtil.ts
@@ -1,8 +1,16 @@
-import { get, keys } from "idb-keyval"
+import { get, keys, set } from "idb-keyval"
 
 export const getCardDataById = async (id: string | string[]) => {
-  const cardData: CardDataType = await get(id)
-  return cardData
+  try {
+    const cardData: CardDataType = await get(id)
+    return cardData
+  } catch (err) {
+    console.trace(
+      "Error getting card",
+      JSON.parse(JSON.stringify(err)),
+      JSON.parse(JSON.stringify(id))
+    )
+  }
 }
 
 export const getAllCardData = async () => {
@@ -11,8 +19,26 @@ export const getAllCardData = async () => {
 
   for (const currentId of ids) {
     const currentCard = await getCardDataById(currentId)
-    cardDataArray.push(currentCard)
+    currentCard && cardDataArray.push(currentCard)
   }
 
   return cardDataArray
+}
+
+export const setCardData = async (cardData: CardDataType) => {
+  try {
+    await set(cardData.id, cardData)
+  } catch (err) {
+    console.trace(
+      "Error setting card data",
+      JSON.parse(JSON.stringify(err)),
+      JSON.parse(JSON.stringify(cardData))
+    )
+  }
+}
+
+export const setAllCardData = async (cardData: CardDataType[]) => {
+  cardData.forEach(async (card) => {
+    await setCardData(card)
+  })
 }
