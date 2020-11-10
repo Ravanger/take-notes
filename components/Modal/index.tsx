871219b4cfb9ead76ad77a94a73bfc8b83f498f0
Ravanger <Boris.Rozzy@gmail.com>
2020-11-10 12:18:02 -0500

    Fix close modal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -11,31 +11,31 @@ type ModalProps = {
 }
 
 const Modal = (props: ModalProps) => {
-  const ref = useRef<HTMLDivElement | null>(null)
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (ref.current && !ref.current.contains(event.target as Node)) {
-      props.closeModal()
-    }
-  }
-
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true)
     return () => {
       document.removeEventListener("click", handleClickOutside, true)
     }
-  }, [])
+  })
+
+  const cardRef = useRef<HTMLDivElement | null>(null)
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+      props.closeModal()
+    }
+  }
 
   return (
     <AnimatePresence>
       {props.isVisible && (
         <motion.div
           key="modal"
+          ref={cardRef}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           css={ModalMain(props.backgroundColor)}
-          ref={ref}
         >
           <h1>{props.id}</h1>
           <p>{props.title}</p>
