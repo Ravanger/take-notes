cf5c100a67a0aea803d599b52b884252abe0a6d5
Ravanger <Boris.Rozzy@gmail.com>
2020-12-17 00:14:04 -0500

    Slight cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
-import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import { StrictMode } from "react"
 import { Provider } from "react-redux"
-import store from "./redux/store"
+
+import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
+import store from "./redux/store"
+
+import "./reset.css"
+import App from "./App"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("root")
 )
 
